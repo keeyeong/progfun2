e3a718000110b779870ccffcc7c625a4b1201aea
Kee Yeong Tan <keeyeong.tan@gmail.com>
2017-01-26 00:07:01 +0100

    first two parts

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -2,12 +2,20 @@ package calculator
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+                   c: Signal[Double]): Signal[Double] = Signal {
+    // Δ = b² - 4ac
+    val B = b()
+    B * B - 4 * a() * c()
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = Signal {
+    // (-b ± √Δ) / 2a
+    if (b() < 0) Set.empty
+    else {
+      val pos = (-b() + delta()) / (2 * a())
+      val neg = (-b() - delta()) / (2 * a())
+      Set(pos, neg)
+    }
   }
 }
